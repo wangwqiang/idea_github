dab06228d7802a0e950f4a3491f5ad057542be60
wangwenqiang <wangwqiang5215@163.com>
2022-06-24 16:01:44 +0800

    第一次更新

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -8,5 +8,6 @@ package com.atguigu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("第一次更新");
     }
 }
