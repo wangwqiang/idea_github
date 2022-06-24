461b6fb151f5e4bd784e4456e45974e4caea97d6
wangwenqiang <wangwqiang5215@163.com>
2022-06-24 16:00:45 +0800

    新建

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author wwq
+ * date 2022/6/24
+ * @since 1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
